78b1f4305eff0858043a5cd5d6dfad4cc7f68da5
Ben <ben.zenittini@gmail.com>
2021-12-29 12:41:49 -0500

    Notifications slide in/out in a direction based on their position.

--- src/components/NotificationGroup.vue
+++ src/components/NotificationGroup.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mw-vn-notification-group">
-        <transition-group name="mw-vn-show-dismiss">
+        <transition-group :name="transformPosition">
             <div v-for="notification in mwVnGroup" v-bind:key="'notification-' + notification.id">
                 <mw-vn-notification v-bind:mw-vn-notification="notification"></mw-vn-notification>
             </div>
@@ -10,14 +10,47 @@
 
 <script lang="ts">
 
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
+import { InternalNotificationConfig } from "../types";
 
 export default defineComponent({
     props: {
         mwVnGroup: Array, // InternalNotificationConfig[]
     },
     setup(props) {
+        let lastTransformPosition = '';
         return {
+            transformPosition: computed(() => {
+                if (props.mwVnGroup!.length === 0) { return lastTransformPosition; }
+
+                // All notifications in this group have the same position. Based on that,
+                // determine which way they should slide "onto" or "off of" the screen.
+                switch ((props.mwVnGroup![0] as InternalNotificationConfig).position) {
+                    case 'top-left':
+                    case 'middle-left':
+                    case 'bottom-left':
+                        lastTransformPosition = 'mw-vn-show-dismiss-left';
+                        return lastTransformPosition;
+
+                    case 'top-right':
+                    case 'middle-right':
+                    case 'bottom-right':
+                        lastTransformPosition = 'mw-vn-show-dismiss-right';
+                        return lastTransformPosition;
+
+                    case 'top-center':
+                        lastTransformPosition = 'mw-vn-show-dismiss-top';
+                        return lastTransformPosition;
+
+                    case 'middle-center':
+                        lastTransformPosition = 'mw-vn-show-dismiss-middle';
+                        return lastTransformPosition;
+
+                    case 'bottom-center':
+                        lastTransformPosition = 'mw-vn-show-dismiss-bottom';
+                        return lastTransformPosition;
+                }
+            }),
         }
     },
 });
@@ -27,10 +60,40 @@ export default defineComponent({
 .mw-vn-notification-group {
     position: fixed;
 
-    .mw-vn-show-dismiss-enter-active,.mw-vn-show-dismiss-leave-active {
+    .mw-vn-show-dismiss-top-enter-active,    .mw-vn-show-dismiss-top-leave-active,
+    .mw-vn-show-dismiss-bottom-enter-active, .mw-vn-show-dismiss-bottom-leave-active,
+    .mw-vn-show-dismiss-left-enter-active,   .mw-vn-show-dismiss-left-leave-active,
+    .mw-vn-show-dismiss-right-enter-active,  .mw-vn-show-dismiss-right-leave-active,
+    .mw-vn-show-dismiss-middle-enter-active, .mw-vn-show-dismiss-middle-leave-active {
         transition: all 0.5s ease;
     }
-    .mw-vn-show-dismiss-enter-from { opacity: 0; transform: translateX(-100px); }
-    .mw-vn-show-dismiss-leave-to   { opacity: 0; transform: translateX(100px); }
+
+    $slide-distance: 100px;
+
+    // Top
+    .mw-vn-show-dismiss-top-enter-from,.mw-vn-show-dismiss-top-leave-to {
+        opacity: 0; transform: translateY(-$slide-distance);
+    }
+
+    // Bottom
+    .mw-vn-show-dismiss-bottom-enter-from,.mw-vn-show-dismiss-bottom-leave-to {
+        opacity: 0; transform: translateY($slide-distance);
+    }
+
+    // Left
+    .mw-vn-show-dismiss-left-enter-from,.mw-vn-show-dismiss-left-leave-to {
+        opacity: 0; transform: translateX(-$slide-distance);
+    }
+
+    // Right
+    .mw-vn-show-dismiss-right-enter-from,.mw-vn-show-dismiss-right-leave-to {
+        opacity: 0; transform: translateX($slide-distance);
+    }
+
+    // Middle
+    .mw-vn-show-dismiss-middle-enter-from,.mw-vn-show-dismiss-middle-leave-to {
+        opacity: 0;
+    }
+
 }
 </style>
